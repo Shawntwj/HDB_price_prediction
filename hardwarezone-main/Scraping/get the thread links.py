04bc047b53d5c28ec5210314992af287710c5a8b
Shawntwj <shawntwj20@gmail.com>
2022-09-06 14:59:33 +0800

    scraping update

--- hardwarezone-main/Scraping/get the thread links.py
+++ hardwarezone-main/Scraping/get the thread links.py
@@ -6,6 +6,9 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
+from pprint import pprint
+
+
 # this file produces thread.csv from links.csv
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
@@ -17,7 +20,7 @@ csvFile = open("threads.csv", "a", newline='', encoding="utf-8")
 csvWriter = csv.writer(csvFile)
 
 # custom check fields to only get threads that have been recently updated or within the month
-datecheck = ["Monday","Tuesday","Wednesday","Thursday","Friday","Yesterday","Sunday","Today","Aug","Saturday"]
+# datecheck = ["Monday","Tuesday","Wednesday","Thursday","Friday","Yesterday","Sunday","Today","Aug","Saturday"]
 
 count = 0
 with open(link_csv_path) as file_handler:
@@ -26,59 +29,40 @@ with open(link_csv_path) as file_handler:
         forum_title = row[0]
 
         driver.get(forum_link)
-        time.sleep(2)
-        print(row[0])
-        
-        max_page = driver.find_elements_by_xpath("//li[@class='pageNav-page ']/a")[-1].get_attribute("innerHTML")
-        print(max_page)
+        time.sleep(2)   
+        max_page = driver.find_element("xpath","//li[@class='pageNav-page ']/a").get_attribute("innerHTML")
 
         for j in range(int(max_page)):
+            if j == 2:
+                break
             time.sleep(2)
-            results = driver.find_elements_by_xpath("//div[@class='structItem-title']/a")
-            results_date = driver.find_elements_by_xpath("//time[@class='structItem-latestDate u-dt']")
-
-            
+            results = driver.find_elements("xpath","//div[@class='structItem-title']/a")
+            results_date = driver.find_elements("xpath","//time[@class='structItem-latestDate u-dt']")
             for i in range(len(results)):
                 count += 1
                 link = results[i].get_attribute("href")
                 thread_title =  results[i].text
-
-                try:
-                    date = results_date[i].text
-                except:
-                    date = "Aug pass"
+                date = results_date[i].text
+                print(link)
                 print(thread_title)
                 print(date)
-                front_date_check = date.split()[0]
-                print(front_date_check)
-                if front_date_check.isdigit() or front_date_check in datecheck:
-                    csvWriter.writerow((forum_title,thread_title,link,date))
-                    recently_updated = True
-                else:
-                    recently_updated = False
-                    pass
-            
-            if recently_updated:
-                pass
-            else:
-                break 
-            
+                csvWriter.writerow((forum_title,thread_title,link,date))
+
             try:
                 print('\n',count, " threads scraped")
                 print("thread page", j+1 ,"done")
-                nextbutton = driver.find_element_by_xpath("//a[@class='pageNav-jump pageNav-jump--next']")
+                nextbutton = driver.find_element("xpath","//a[@class='pageNav-jump pageNav-jump--next']")
                 nextbutton.click()
 
             except:
                 time.sleep(30)
-                nextbutton = driver.find_element_by_xpath("//a[@class='pageNav-jump pageNav-jump--next']")
+                nextbutton = driver.find_element("xpath","//a[@class='pageNav-jump pageNav-jump--next']")
                 nextbutton.click()
 
-            # print("all thread done")
+            print("all thread done")
             # break
 
         print("topic ", forum_title, " done")
-
 
 
 
