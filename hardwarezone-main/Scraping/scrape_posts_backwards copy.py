9810ff9f84465516b3803911f32399611d0bcee7
Shawntwj <shawntwj20@gmail.com>
2022-09-08 19:14:08 +0800

    hardwarezone scraping done

--- hardwarezone-main/Scraping/scrape_posts_backwards copy.py
+++ hardwarezone-main/Scraping/scrape_posts_backwards copy.py
@@ -21,8 +21,8 @@ driver = webdriver.Chrome(PATH)
 # doneLinkFile = open("thread p.csv", "w", newline='', encoding="utf-8")
 # csvFile = open("posts.csv", "w", newline='', encoding="utf-8")
 # ------comment out above line and use the below line if you wish to add on to existing excel file and vice versa----------
-csvFile = open("posts.csv", "a", newline='', encoding="utf-8-sig")
-doneLinkFile = open("thread done.csv", "a", newline='', encoding="utf-8-sig")
+csvFile = open("posts.csv", "a", newline='', encoding="utf-8")
+doneLinkFile = open("thread p.csv", "a", newline='', encoding="utf-8")
 
 doneLinkcsvWriter = csv.writer(doneLinkFile)
 csvWriter = csv.writer(csvFile)
@@ -71,41 +71,90 @@ with open(thread_csv_file, encoding="utf8") as file_handler:
         for i in range(int(max_page)):
             time.sleep(5)
             results = driver.find_elements("xpath","//div[@class='message-inner']")
+
             # loop according to the number of posts in one page
             for result in results:
                 # scrape the information from the post
                 try:
                     membername = result.find_element("xpath",".//h4[@class='message-name']").text
-                    memberlink = result.find_element("xpath",".//h4[@class='message-name']/a").get_attribute("href")
-                    usertitle = result.find_element("xpath",".//h5[@class='userTitle message-userTitle']").text
-                    content = result.find_element("xpath",".//div[@class='bbWrapper']").text
-                    date = result.find_element("xpath",".//time").text
+                    memberlink = result.find_element("xpath"".//h4[@class='message-name']/a").get_attribute("href")
+                    usertitle = result.find_element("xpath"".//h5[@class='userTitle message-userTitle']").text
+                    content = result.find_element("xpath"".//div[@class='bbWrapper']").text
+                    date = result.find_element("xpath"".//time").text
 
-                    csvWriter.writerow((topic_name, thread_title, thread_link ,membername,memberlink,usertitle,date,content))
+                    # reformat date as it is in a weird string format with words like today tuesday wednesday ect
+                    try:
+                        date_list = date.split(",")
+                        month = date_list[0].split()[0]
+                        day= date_list[0].split()[1]
+                        year = date_list[1].strip()
+                        date_edited = '/'.join((day,month,year))
+
+                        # month control dates in this section are considered to be more than a week old
+                        this_week = False
+
+                        if month == "Aug":
+                            print(month)
+                            this_month = True
+                        else:
+                            print(month)
+                            this_month = False
+
+                    
+                    # reformat and change the days name into dates with words like today tuesday wednesday ect
+                    except:
+                        try:
+                            # uses the datedict above to convert day to dates
+                            dayname = date.split()[0]
+                            date_edited = datedict[dayname]
+                            # week control
+                            this_week = True
+                        except:
+                            # covers numbers eg "3 mins ago" and "moments ago"
+                            date_edited = datedict["Today"]
+                            # week control
+                            this_week = True
+
+                    # week control stop scraping the posts after this and exit page
+                    if this_week or this_month:
+                        pass
+                    else:
+                        break 
+
+                    csvWriter.writerow((topic_name, thread_title, thread_link ,membername,memberlink,usertitle,date_edited,content))
 
                 # skip post if there is missing information Eg deleted content
                 except:
                     pass
                 
-#                 # week control stop scraping the thread and exit thread
-#                 if this_week:
-#                     pass
-#                 else:
-#                     break 
+                # week control stop scraping the thread and exit thread
+                if this_week:
+                    pass
+                else:
+                    break 
             print("thread page", i+1 ,"done")
 
+            # decide whether you want to continue scraping further based on week or month due to time constraints i am filtering by the past week
+
+            if this_week:
+                pass
+            else:
+                break 
+
             try: 
-                nextbutton = driver.find_element("xpath","//a[@class='pageNav-jump pageNav-jump--prev']")
+                nextbutton = driver.find_element_by_xpath("//a[@class='pageNav-jump pageNav-jump--prev']")
+                
                 nextbutton.click()
             except:
                 print("all of thread done")
                 break
-            
+
         count+=1
         print(count, " threads done")
+
     print("---------------all of topics done---------------")
 
-# csvFile.close()
-# driver.close()
+csvFile.close()
+driver.close()
 
 # plan is to only extract out the last page of the forums
